38ddd63a4f5cb8bf311c3e8102cb8d1c2d3ebc63
ashimura <ashimura@w3.org>
2020-04-13 22:06:22 +0900

    add wot-architecture

---
+++ wot-architecture/conv.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+use strict;
+use utf8;
+use HTML::TreeBuilder;
+use HTML::TreeBuilder::XPath;
+use HTML::Selector::XPath qw/selector_to_xpath/;
+use Encode::Guess;
+use File::Copy;
+
+### Read HTML
+my $filename = $ARGV[0];
+open( IN, "<$filename" );
+local $/ = undef;
+my $html = <IN>;
+close(IN);
+
+### Conversion
+my $h = HTML::TreeBuilder->new;
+$h->parse($html);
+$h->eof;
+
+# remove extra <font> tags
+for my $node ($h->findnodes('//font')){
+    $node->replace_with_content();
+}
+# remove extra <span> tags with 'style' attribute
+for my $node ($h->findnodes('//span')){
+    $node->replace_with_content for $node->look_down(style => "background: #ffffff");
+}
+# remove extra <span> tags with 'lang' attribute
+for my $node ($h->findnodes(q{//span[@lang='ja-JP']})){
+    if (!$node->is_empty()) {
+        $node->replace_with_content();
+    }
+}
+# remove extra 'align', 'class' and 'style' attributes from <p> tags
+for my $node ($h->findnodes('//p')){
+    $node->attr('class' => undef) for $node->look_down(class => "western");
+    $node->attr('align' => undef) for $node->look_down(align => "justify");
+    $node->attr('style' => undef) for $node->look_down(style => "margin-left: 0.09in; margin-right: 0.07in");
+}
+
+$html = $h->as_HTML("<>&", "  ",{});
+$h->delete;
+
+### Write fixed HTML
+open(OUT,">$filename");
+print OUT $html;
+close(OUT);
---
+++ wot-architecture/conv.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp Web\ of\ Things\ \(WoT\)\ Architecture_v0.2.html Overview.html
+./conv.pl Overview.html
